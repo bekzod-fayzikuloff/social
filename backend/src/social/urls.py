5e3a0198ee1ff9fb3ca38ef3e95458ecca5c3e93
bekzod-fayzikuloff <fayzikuloff.b@gmail.com>
2022-04-10 20:56:05 +0300

    Backend: Add OpenAPI documentation support

--- backend/src/social/urls.py
+++ backend/src/social/urls.py
@@ -14,14 +14,39 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title=settings.DOCS_TITLE,
+        default_version=settings.DOCS_TITLE,
+        description=settings.DOCS_DESCRIPTION,
+        terms_of_service=settings.DOCS_TERMS_OF_SERVICE,
+        contact=openapi.Contact(email=settings.DOCS_CONTACT),
+        license=openapi.License(name=settings.DOCS_LICENSE),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 api_routes = [
     path("", include("apps.auth.urls")),
 ]
 
+docs_patterns = [
+    re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    re_path(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    re_path(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+]
+
 urlpatterns = [
+    path("", include(docs_patterns)),
     path("api/", include(api_routes)),
     path("admin/", admin.site.urls),
 ]
